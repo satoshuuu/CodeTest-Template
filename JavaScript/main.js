06ff7180aa28be1d5245ec6e251681ba67fe8677
佐藤周平 <s.s.odn.0617@gmail.com>
2020-06-14 14:42:40 +0900

    add Javascript

---
+++ JavaScript/main.js
@@ -0,0 +1,17 @@
+//JavaScriptでの標準入力のテンプレート
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+reader.on('line', (line) => {
+  lines.push(line);
+});
+
+reader.on('close', () => {
+  //ここに動作を書く
+});
